1e0cfb5d8943deaf8d4d87d0fb4adae8267a25ce
tanvirkhan00 <kmtanvir1111@gmail.com>
2024-09-12 01:24:54 +0600

    add responsive and correction project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,46 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm:"480px",
+      md:"768px",
+      lg:"1024px"
+    },
+    extend: {
+      colors: {
+        tillColor:'#008080',
+        darkPurpelColor:'#301934',
+        yellowColor:'#DFFF00',
+        midnightBlueColor:'#191970',
+        whiteColor:'#fff',
+        blackColor:'#000',
+        greenColor:'#007936',
+        redColor:'#cc3433',
+        darkColor:'#000',
+        darkColorLight:'#171717',
+        limeColor:'#00FF00',
+        blueColor:'#0000FF',
+        skyBlueColor:'#87CEEB',
+        brownColor:'#A52A2A',
+        coffeeColor:'#6F4E37',
+        grayColor:'#A9A9A9',
+        platinumColor:'#E5E4E2',
+        darkPinkColor:'#AA336A'
+      },
+    },
+    container:{
+      center:true,
+      padding: {
+        DEFAULT:'1rem',
+        sm: '1.5rem',
+      }
+    },
+    fontFamily:{
+      fontSuse: ["SUSE", 'sans-serif'],
+      fontProtest: ["Protest Guerrilla", 'sans-serif'],
+      fontLobster: ["Lobster", 'sans-serif'],
+      fontBebas: ["Bebas Neue", 'sans-serif']
+    }
   },
   plugins: [],
 }
